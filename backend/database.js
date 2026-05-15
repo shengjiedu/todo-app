987cf20760aadf44239fcff0a4ee44cb9a9ce93e
Todo App Developer <user@todo-app.local>
2026-05-15 20:27:15 +0800

    chore: add Fly.io deployment config (Dockerfile, fly.toml, data volume support)

--- backend/database.js
+++ backend/database.js
@@ -5,7 +5,9 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dbPath = path.join(__dirname, 'todo.db');
+const dbPath = process.env.FLY_VOLUME_DIR
+  ? path.join('/data', 'todo.db')
+  : path.join(__dirname, 'todo.db');
 const db = new Database(dbPath);
 
 db.pragma('journal_mode = WAL');
